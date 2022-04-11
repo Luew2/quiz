1436cb5e9a6315bb60018e629cd3015b6f93f189
Lucas Ewing <lucas.ewing2000@gmail.com>
2022-04-11 16:18:27 -0700

    Re-organized code

--- main.go
+++ main.go
@@ -9,28 +9,57 @@ import (
 	"time"
 )
 
-func main() {
-	csvFile := flag.String("csv", "problems.csv",
+var csvFile *string
+var timer *int
+var problems []problem
+
+func init() {
+	csvFile = flag.String("csv", "problems.csv",
 		"a csv file in the format 'queston,answer' for your quiz!")
-	timer := flag.Int("timer", 60, "an integer input in terms of seconds for time limiting the quiz.")
+	timer = flag.Int("timer", 60, "an integer input in terms of seconds for time limiting the quiz.")
 	flag.Parse()
-	file, err := os.Open(*csvFile)
-	if err != nil {
-		exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFile))
-	}
-	quiz(*timer, file)
 }
 
-func quiz(t int, f *os.File) {
+func main() {
+	file := loadProblem(*csvFile)
+	parse(file)
+	quiz(*timer)
+}
+
+func loadProblem(csv string) *os.File {
+	file, err := os.Open(csv)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s", csv))
+	}
+	return file
+
+}
+
+func parse(f *os.File) {
 	r := csv.NewReader(f)
 	lines, err := r.ReadAll()
 	if err != nil {
 		exit("Failed to parse CSV file")
 	}
-	problems := parseLines(lines)
-	correct := 0
+	problems = parseLines(lines)
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+func quiz(t int) {
 	fmt.Printf("You have %v seconds to finish this quiz:\n", t)
+	correct := 0
 	stopWatch := time.NewTimer(time.Duration(t) * time.Second)
+
 problemLoop:
 	for i, p := range problems {
 		fmt.Printf("problem #%d: %s\n", i+1, p.q)
@@ -54,17 +83,6 @@ problemLoop:
 		}
 	}
 	fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
-}
-
-func parseLines(lines [][]string) []problem {
-	ret := make([]problem, len(lines))
-	for i, line := range lines {
-		ret[i] = problem{
-			q: line[0],
-			a: strings.TrimSpace(line[1]),
-		}
-	}
-	return ret
 }
 
 type problem struct {
