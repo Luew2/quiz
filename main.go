8dd66fc66b6339428f268c34c9f25b022811d96b
Lucas Ewing <lucas.ewing2000@gmail.com>
2022-04-11 15:51:58 -0700

    My final solution

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	csvFile := flag.String("csv", "problems.csv",
+		"a csv file in the format 'queston,answer' for your quiz!")
+	timer := flag.Int("timer", 60, "an integer input in terms of seconds for time limiting the quiz.")
+	flag.Parse()
+	file, err := os.Open(*csvFile)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFile))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse CSV file")
+	}
+	problems := parseLines(lines)
+	correct := 0
+	fmt.Printf("You have %v seconds to finish this quiz:\n", *timer)
+	stopWatch := time.NewTimer(time.Duration(*timer) * time.Second)
+problemLoop:
+	for i, p := range problems {
+		fmt.Printf("problem #%d: %s\n", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-stopWatch.C:
+			fmt.Println()
+			break problemLoop
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+				fmt.Printf("Correct! Total number correct: %d\n", correct)
+			} else {
+				fmt.Printf("Incorrect! Total number correct: %d\n", correct)
+			}
+		}
+	}
+	fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
